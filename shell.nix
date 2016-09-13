f512b5dfeba1c0683964cbdf59c270cf7a7c70c7
Robert Hensing <robert@roberthensing.nl>
2016-09-14 00:05:51 +0200

    shell.nix: add intero

--- shell.nix
+++ shell.nix
@@ -1,3 +1,6 @@
 { nixpkgs ? import ./nixpkgs {} }:
 let p = import ./packages.nix { inherit nixpkgs; };
-in p.helium.env
+    override = obj: f: derivation (obj.drvAttrs // f (obj.drvAttrs));
+in  (override p.helium.env (super : {
+          nativeBuildInputs = super.nativeBuildInputs ++ [p.haskellPackages.intero];
+                    }))
